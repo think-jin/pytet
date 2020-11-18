1fae9eb19565cad80c0b37d00823e91e963bbed0
think-jin <thingjin@naver.com>
2020-11-19 00:07:04 +0900

    pytet added

---
+++ pytet.py
@@ -0,0 +1,147 @@
+from matrix import *
+import random
+
+def draw_matrix(m):
+    array = m.get_array()
+    for y in range(m.get_dy()):
+        for x in range(m.get_dx()):
+            if array[y][x] == 0:
+                print("□", end='')
+            elif array[y][x] == 1:
+                print("■", end='')
+            else:
+                print("XX", end='')
+        print()
+
+def Transpose(matrix): # 'w' 키 입력시 현재 블록을 시계방향 90도 회전과 충돌시 반시계반향 90도 회전하게 하는 함수 구현
+    n = len(matrix)
+    tp = [[0] * n for _ in range(n)]
+    for r in range(n):
+        for c in range(n):
+            tp[c][n-1-r] = matrix[r][c]
+    return tp
+###
+### initialize variables
+###     
+arrayBlk_lst = [ [[ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ], [ 0, 0, 1, 0 ]], 
+             [[ 0, 1, 0 ], [ 0, 1, 0 ], [ 1, 1, 0 ]],
+             [[ 0, 1, 0 ], [ 0, 1, 0 ], [ 0, 1, 1 ]],
+             [[ 1, 1 ], [ 1, 1 ]],
+             [[ 0, 1, 0 ], [ 0, 1, 1 ], [ 0, 0, 1 ]],
+             [[ 0, 0, 0 ], [ 1, 1, 1 ], [ 0, 1, 0 ]],
+             [[ 0, 0, 0 ], [ 1, 1, 0 ], [ 0, 1, 1 ]] ] #테트리스 블록 리스트 생성(I, J, L, O, S, T, Z)
+arrayBlk = arrayBlk_lst[random.randrange(0, 7)] # 초기 블록 : 7가지 블록 중 하나를 무작위로 선택함
+
+### integer variables: must always be integer!
+iScreenDy = 15
+iScreenDx = 10
+iScreenDw = 4
+top = 0
+left = iScreenDw + iScreenDx//2 - 2
+
+newBlockNeeded = False
+
+arrayScreen = [
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] ]
+
+###
+### prepare the initial screen output
+###  
+iScreen = Matrix(arrayScreen)
+oScreen = Matrix(iScreen)
+currBlk = Matrix(arrayBlk)
+tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+tempBlk = tempBlk + currBlk
+oScreen.paste(tempBlk, top, left)
+draw_matrix(oScreen); print()
+
+###
+### execute the loop
+###
+
+while True:
+    key = input('Enter a key from [ q (quit), a (left), d (right), s (down), w (rotate), \' \' (drop) ] : ')
+    if key == 'q':
+        print('Game terminated...')
+        break
+    elif key == 'a': # move left
+        left -= 1
+    elif key == 'd': # move right
+        left += 1
+    elif key == 's': # move down
+        top += 1
+    elif key == 'w': # rotate the block clockwise
+        arrayBlk = Transpose(arrayBlk) # arrayBlk 갱신
+        currBlk = Matrix(arrayBlk) # currBlk 갱신
+    elif key == ' ': # drop the block  
+        while (tempBlk.anyGreaterThan(1) is False): # temp.Blk.anyGreaterThan(1) == True -> 탈출(break)
+            top +=1 # 무한루프 생성, top+=1(현재 블록 추락)반복
+            tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx()) # tempBlk 갱신
+            tempBlk = tempBlk + currBlk # tempBlk 갱신
+    else:
+        print('Wrong key!!!')
+        continue
+
+    tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+    tempBlk = tempBlk + currBlk
+    if tempBlk.anyGreaterThan(1):
+        if key == 'a': # undo: move right
+            left += 1
+        elif key == 'd': # undo: move left
+            left -= 1
+        elif key == 's': # undo: move up
+            top -= 1
+            newBlockNeeded = True
+        elif key == 'w': # undo: rotate the block counter-clockwise
+            for i in range(3):
+                arrayBlk = Transpose(arrayBlk) # 블록에 부딪힐 경우, 90도로 3번더(270도) 돌림 -> 원위치(90 + 270 = 360)
+            currBlk = Matrix(arrayBlk) # currBlk갱신
+        elif key == ' ': # undo: move up
+            top -= 1 # 위에있는 반복문에서 탈출한후, top-=1
+            newBlockNeeded = True # newBlockNeeded = True -> 무작위로 선택된 새 블록 출현
+
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+
+    oScreen = Matrix(iScreen)
+    oScreen.paste(tempBlk, top, left)
+    draw_matrix(oScreen); print()
+
+    if newBlockNeeded:
+        iScreen = Matrix(oScreen)
+        top = 0
+        left = iScreenDw + iScreenDx//2 - 2
+        newBlockNeeded = False
+        arrayBlk = arrayBlk_lst[random.randrange(0, 7)] #새 블록 출현시 7가지 블록 중 하나를 무작위로 선택함
+        currBlk = Matrix(arrayBlk)
+        tempBlk = iScreen.clip(top, left, top+currBlk.get_dy(), left+currBlk.get_dx())
+        tempBlk = tempBlk + currBlk
+        if tempBlk.anyGreaterThan(1):
+            print('Game Over!!!')
+            break
+        
+        oScreen = Matrix(iScreen)
+        oScreen.paste(tempBlk, top, left)
+        draw_matrix(oScreen); print()
+        
+###
+### end of the loop
+###
